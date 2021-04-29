15197825bb62fd64e3b57f44e08e07acc9f24aa3
Steven A. Zahm <shazahm1@hotmail.com>
2021-04-29 16:18:58 -0400

    Update connections-category-homepage.php

--- connections-category-homepage.php
+++ connections-category-homepage.php
@@ -244,10 +244,9 @@ if ( ! class_exists( 'Connections_Category_Homepage' ) ) {
 
 			add_action(
 				'admin_notices',
-				create_function(
-					'',
-					'echo \'<div id="message" class="error"><p><strong>ERROR:</strong> Connections must be installed and active in order use Connections Category Homepage.</p></div>\';'
-				)
+				function() {
+					echo '<div id="message" class="error"><p><strong>ERROR:</strong> Connections must be installed and active in order use Connections Category Homepage.</p></div>';
+				}
 			);
 
 			return FALSE;
